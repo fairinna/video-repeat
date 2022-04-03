57aa6347f39a98b8c8c5b91bbc8d5320f11ad275
Inna Miachikova <inna.namatov@gmail.com>
2022-04-03 18:29:54 +0300

    create searhBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    state = {mySearch:''}
+
+    myChangeInput = (event)=>{
+        this.setState({mySearch:event.target.value})
+    }
+
+    onSubmitForm=(event)=>{
+        event.preventDefault();
+        // TODO: Make shure we call
+        // callback from parent component
+    }
+
 
     render(){
         return (
-            <div> SearchBar</div>
+            <div className='ui segment search-bar'> 
+            <form action="" className='ui form' onSubmit={this.onSubmitForm}>
+                <div className="field">
+                    <label htmlFor="">Search Video</label>
+                    <input type="text" value={this.state.mySearch} onChange={this.myChangeInput}/>
+                </div>
+            </form>
+            
+            </div>
 
         )
         }
